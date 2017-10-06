da8dbbac7b17a57d567825d803c1af109021d534
stevenpack <steven_john_pack@yahoo.com.au>
2017-10-06 00:01:44 -0700

    poc of extracting blockchain from state and accessing with closure

--- src/web.rs
+++ src/web.rs
@@ -1,5 +1,3 @@
-#![feature(plugin, decl_macro)]
-#![plugin(rocket_codegen)]
 extern crate rocket;
 
 use rocket::State;
@@ -20,13 +18,24 @@ impl BlockchainState {
 
 //todo: post
 #[get("/new-block")]
-pub fn new_block(state: State<BlockchainState>) -> String {
-    //let mut blockchain = state.blockchain.write().expect("Unable to get write lock");
+pub fn new_block(state: State<BlockchainState>) -> Result<String, u32> {
+    //let mut blockchain = state.blockchain.write().expect("Unable to get write lock");    
+    // let guard = state.blockchain.write();
+    // if guard.is_ok() {
+    //     return Ok(guard.unwrap().new_block())
+    // }
+    return x(&state,  &mut |b| b.new_block() );
+}
+
+fn x<F>(state: &State<BlockchainState>, blockchain_op: &mut F) -> Result<String, u32> 
+    where F: Fn(&mut Blockchain) -> String {
     let guard = state.blockchain.write();
-    if guard.is_ok() {
-        return guard.unwrap().new_block()
+    if guard.is_ok() {        
+        let mut blockchain = guard.unwrap();
+        let result = blockchain_op(&mut blockchain);
+        return Ok(result);        
     }
-    "nope".to_string()
+    Err(500)
 }
 
 //todo: post
