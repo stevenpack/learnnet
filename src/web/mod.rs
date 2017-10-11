32fa933ebb7ed4a264ee12f846ef249ac3dde356
stevenpack <steven_john_pack@yahoo.com.au>
2017-10-11 01:11:13 -0700

    registering nodes

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -4,7 +4,7 @@ use rocket::State;
 use lib::blockchain::*;
 use lib::transaction::*;
 use std::sync::{RwLock};
-
+use url::{Url};
 mod converters;
 
 pub struct BlockchainState {
@@ -41,10 +41,16 @@ struct ChainResult<'a> {
     length: usize
 }
 
+#[derive(Serialize)]
+struct RegisterNodeResponse {
+    message: String,
+    total_nodes: usize
+}
+
 //todo: respone as JSON - https://github.com/SergioBenitez/Rocket/blob/v0.3.3/examples/json/src/main.rs
 #[get("/mine", format = "application/json")]
 pub fn mine(state: State<BlockchainState>) -> Result<String, u32> {
-    return blockchain_op(&state, |b| {
+    blockchain_op(&state, |b| {
 
         let mined_block = b.mine();
         let response = MineResult {
@@ -55,24 +61,24 @@ pub fn mine(state: State<BlockchainState>) -> Result<String, u32> {
             previous_hash: mined_block.previous_hash.clone()
         };
 
-        serde_json::to_string(&response).unwrap_or_else(|e| {
+        Ok(serde_json::to_string(&response).unwrap_or_else(|e| {
             error!("serialize error: {:?}", e);
             return String::from("Block mined, but details not available")
-        })
-    });
+        }))
+    })
 }
 
 #[post("/transaction/new", format = "application/json", data = "<transaction>")]
 pub fn new_transaction(transaction: Transaction, state: State<BlockchainState>) -> Result<String, u32> {
     blockchain_op(&state, |b| {
         let index = b.new_transaction(transaction.clone());
-        return format!("Transaction added at block {}", index);
+        return Ok(format!("Transaction added at block {}", index));
     })
 }
 
 #[get("/chain", format = "application/json")]
 pub fn chain(state: State<BlockchainState>) -> Result<String, u32> {
-    return blockchain_op(&state, |b| {
+    blockchain_op(&state, |b| {
 
             let chain = b.chain();
             let response = ChainResult {
@@ -80,20 +86,44 @@ pub fn chain(state: State<BlockchainState>) -> Result<String, u32> {
               length: chain.len()
             };
 
-            serde_json::to_string(&response).unwrap_or_else(|e| {
+            Ok(serde_json::to_string(&response).unwrap_or_else(|e| {
                 error!("serialize error: {:?}", e);
                 return String::from("Could not serialize chain.")
-            })
-        });
+            }))
+        })
 }
 
 #[post("/nodes/register", format = "application/json", data="<node_list>")]
 pub fn register_node(node_list: NodeList, state: State<BlockchainState>) -> Result<String, u32> {
-    blockchain_op(&state, |b| {
-               
-        //let url = Url::parse();
-        //let index = b.register_node(url);
-        return format!("Added nodes");
+    return blockchain_op(&state, |b| {
+
+        let mut node_urls = Vec::<Url>::with_capacity(node_list.nodes.len());
+
+        //Validate - all or nothing
+        for node in &node_list.nodes {
+           let parse_result = Url::parse(node);
+           if parse_result.is_err() {
+               warn!("Failed to parse {} {:?}", node, parse_result.err());
+               return Err(400, /* all nodes must be valid */)
+           }
+           let url = parse_result.expect("validated");
+           node_urls.push(url);
+        }
+
+        //Add
+        for node_url in node_urls {
+            b.register_node(node_url);
+        }      
+
+        let response = RegisterNodeResponse {
+            message: String::from("New nodes have been added"),
+            total_nodes: b.nodes().len(),
+        };
+
+        Ok(serde_json::to_string(&response).unwrap_or_else(|e| {
+                error!("serialize error: {:?}", e);
+                return String::from("Could not serialize chain.")
+        }))       
     })
 }
 
@@ -101,12 +131,12 @@ pub fn register_node(node_list: NodeList, state: State<BlockchainState>) -> Resu
 /// Retrieves the blockchain from state, unlocks and executes the closure
 /// 
 fn blockchain_op<F>(state: &State<BlockchainState>, blockchain_op: F) -> Result<String, u32> 
-    where F: Fn(&mut Blockchain) -> String {
+    where F: Fn(&mut Blockchain) -> Result<String, u32> {
     let guard = state.blockchain.write();
     if guard.is_ok() {        
         let mut blockchain = guard.unwrap();
         let result = blockchain_op(&mut blockchain);
-        return Ok(result);        
+        return result;
     }
     error!("Couldn't acquire lock");
     Err(500)
